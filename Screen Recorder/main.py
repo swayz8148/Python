880e6edb5ff92aede40ed2f798e676eb36869b9e
Swayz <michealdownard11@gmail.com>
2023-07-04 09:27:09 +0100

    Update: can use more monitors now have fun

--- Screen Recorder/main.py
+++ Screen Recorder/main.py
@@ -1,41 +1,66 @@
 import cv2
-import pyautogui
 import numpy as np
+import datetime
+from mss import mss
+from screeninfo import get_monitors
 
-# Screen resolution
-screen_size = pyautogui.size()
+# Choose the number of monitors to capture
+num_monitors = int(input("Enter the number of monitors to capture: "))
 
-# Output file name and codec
-output_file = 'screen_record.mp4'
+# Get information about all available monitors
+monitors = get_monitors()
+if len(monitors) < num_monitors:
+    num_monitors = len(monitors)
+
+# Generate the current date and time string
+current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# Output file codec and frame rate
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 fps = 20.0  # Adjust the frame rate as needed
-output = cv2.VideoWriter(output_file, fourcc, fps, screen_size)
+
+# Create separate video writers for each monitor
+output_writers = []
+for monitor_index in range(num_monitors):
+    monitor = monitors[monitor_index]
+    screen_size = (monitor.width, monitor.height)
+    output_file = f'screen_record_{current_datetime}_{monitor_index}.mp4'
+    output = cv2.VideoWriter(output_file, fourcc, fps, screen_size)
+    output_writers.append(output)
 
 # Record screen until 'q' key is pressed
-while True:
-    try:
-        # Capture screen image
-        img = pyautogui.screenshot()
+with mss() as sct:
+    while True:
+        try:
+            for monitor_index in range(num_monitors):
+                monitor = monitors[monitor_index]
 
-        # Convert the image to a numpy array representation
-        frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+                # Capture screen image for the current monitor
+                monitor_dict = {
+                    "left": monitor.x,
+                    "top": monitor.y,
+                    "width": monitor.width,
+                    "height": monitor.height,
+                }
+                img = np.array(sct.grab(monitor_dict))
 
-        # Apply slight image smoothing
-        frame = cv2.bilateralFilter(frame, 5, 50, 50)
+                # Convert the image to a numpy array representation
+                frame = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)  # Convert color space from BGRA to BGR
 
-        # Write the frame to the output file
-        output.write(frame)
+                # Write the frame to the output file for the current monitor
+                output_writers[monitor_index].write(frame)
 
-        # Display the resulting frame
-        cv2.imshow('Screen Recording', frame)
+                # Display the resulting frame
+                cv2.imshow('Screen Recording', frame)
 
-        # Stop recording when 'q' is pressed
-        if cv2.waitKey(1) == ord('q'):
+            # Stop recording when 'q' is pressed
+            if cv2.waitKey(1) == ord('q'):
+                break
+        except Exception as e:
+            print(f"An error occurred: {e}")
             break
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        break
 
-# Release the video writer and close the window
-output.release()
+# Release the video writers and close the window
+for output in output_writers:
+    output.release()
 cv2.destroyAllWindows()
